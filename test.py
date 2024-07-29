8052651476556626c43cc2229086962f4006414d
Suherlina <99602260+barbelina@users.noreply.github.com>
2024-07-30 04:45:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
 
 # Assuming kroscek is your DataFrame
 # Example DataFrame for demonstration
@@ -17,4 +18,4 @@ letters = ['Selisih Besar', 'Normal']
 # Apply np.select with correct conditions and choices
 kroscek['SELISIH 100%'] = np.select(conditions_100, letters, default='Undefined')
 
-print(kroscek)
+st.write(kroscek)
